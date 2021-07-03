4a83fda7152cede07373e544631c43f6d8ec5511
NzrSrd <nizar.sarieldin@gmail.com>
2021-07-03 03:16:30 +0200

    the implementation of the reverse proxy server for the OpenWeatherMap api

---
+++ README.md
@@ -0,0 +1,3 @@
+### Reverse Proxy Server
+
+A Reverse Proxy server build with NodeJs. It's a solution to the CORS by forwarding the api request to server which will make the request and return the client the data. 
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config()
+
+app.use(cors({
+  origin: '*'
+}))
+
+
+
+app.get(':endpoint([\\/\\w\\.-]*)', function(req, res) {
+
+  let endpoint = process.env.API_BASE_URL + req.params.endpoint;
+
+  let params = {};
+
+  if(!!process.env.APP_ID_PARAM_NAME && !!process.env.APP_ID) {
+    params[process.env.APP_ID_PARAM_NAME] = process.env.APP_ID
+  }
+
+  for(const [filed, value] of Object.entries(req.query)) {
+    params[field] = value
+  }
+
+  axios
+    .get(endpoint, {
+      params: params
+    }).then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      res.json(error)
+    })
+})
+
+//PORT
+const port = process.env.PORT || 4000;
+app.listen(port, () => {console.log(`OpenWeatherMap API listening ${port}...`)});
