3274be59d39679d029a8c7f21612bddf10112095
NzrSrd <nizar.sarieldin@gmail.com>
2021-07-03 04:44:32 +0200

    update

--- index.js
+++ index.js
@@ -38,3 +38,4 @@ app.get(':endpoint([\\/\\w\\.-]*)', function(req, res) {
 //PORT
 const port = process.env.PORT || 4000;
 app.listen(port, () => {console.log(`OpenWeatherMap API listening ${port}...`)});
+
